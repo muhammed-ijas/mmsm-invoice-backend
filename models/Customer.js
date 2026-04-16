3a814d1eef691a2816791f7fd5c8d201db30bbff
ijas <muhammedijas793@gmail.com>
2026-04-16 14:27:53 +0530

    added customer ac nuner optiona

--- models/Customer.js
+++ models/Customer.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const customerSchema = new mongoose.Schema({
   name: { type: String, required: true },
   vatNumber: { type: String }, // Optional
-  accountNo: { type: String, unique: true, sparse: true }, // Optional but unique if provided
+  accountNo: { type: String }, 
   phone: { type: String }, // Optional
   address: { type: String } // Optional
 }, { timestamps: true });
